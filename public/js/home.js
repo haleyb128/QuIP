2d2c4962903bc552932eb0235d065a9cf921b42e
Joe Gathje <j.gathje6@gmail.com>
2018-07-31 13:45:10 -0500

    minor changes

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
                     console.log(data[i])
                     $("#quote-full").text(data[i].Quote)
                     $("#author-name").text(data[i].Author)
+                    $("#quote-full2").text(data[i].Quote)
+                    $("#author-name2").text(data[i].Author)
                 }
             }
         })

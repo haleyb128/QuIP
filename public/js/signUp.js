98e7107541dcc0f2ad9d94bf667fba05295b8b0a
Joe Gathje <j.gathje6@gmail.com>
2018-08-01 13:06:50 -0500

    script changes

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,19 +1,31 @@
 $(document).ready(function () {
-    // localStorage.setItem("loggedin", "no")
-    function randomQuote() {
-        $.get("/api/quotes", function (data) {
-            let randomNum = Math.floor(Math.random() * data.length)
+    $("#signUp").on("click", function (event) {
+        event.preventDefault();
+        let name = $("#sign-username").val().trim();
+        let password = $("#sign-password").val().trim();
+        let compareName;
+        $.get("/api/signin", function (data) {
             for (let i = 0; i < data.length; i++) {
-                if (i === randomNum) {
-                    console.log(data[i])
-                    $("#quote-full").text(data[i].Quote)
-                    $("#author-name").text(data[i].Author)
-                    $("#quote-full2").text(data[i].Quote)
-                    $("#author-name2").text(data[i].Author)
+                if (data[i].name === name) {
+                    compareName = name;
+                    return compareName;
                 }
             }
+        }).then(function () {
+            console.log(name)
+            console.log(compareName)
+            if (compareName === name) {
+                alert("Name taken!")
+            }
+            else {
+                $.post("/api/createuser", {
+                    name: name,
+                    password: password
+                })
+                console.log("SUCCESS!")
+            }
         })
-    }
+    })
     $("#userSubmit").on("click", function (event) {
         event.preventDefault();
         let userName = $("#exampleInputEmail2").val().trim();
@@ -56,10 +68,6 @@ $(document).ready(function () {
             else if (userObj.password != password) {
                 alert("WRONG PASSWORD")
             }
-
         })
     })
-    randomQuote();
-
 })
-
